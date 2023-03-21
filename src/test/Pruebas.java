4f8e2beeb008d0359e800972fc3054b42ba5096c
josele11 <joseantonio.triches@campusfp.es>
2023-03-21 10:21:58 +0100

    first commit

---
+++ src/test/Pruebas.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Main.CuentaBancaria;
+
+class Pruebas {
+
+	CuentaBancaria cb = new CuentaBancaria();
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
